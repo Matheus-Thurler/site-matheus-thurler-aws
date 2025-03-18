8d52697298fdabbf10dcab3f191b7d39d47835f4
Matheus Thurler <matheusthurlernf@gmail.com>
2025-03-18 19:11:47 -0300

    fix sonar

--- .github/workflows/sonar/index.ts
+++ .github/workflows/sonar/index.ts
@@ -9,7 +9,7 @@ async function run() {
     const sonarProjectKey = core.getInput('sonar-project-key');
 
     core.info('Executando ESLint');
-    await exec('npx eslint . --config .github/workflows/sonar/eslint.config.js || true);
+    await exec('npx eslint . --config .github/workflows/sonar/eslint.config.js || true');
 
     core.info('Instalando SonarScanner');
     await exec('npm install -g sonarqube-scanner');
