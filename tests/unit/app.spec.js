3a25fcae1bbd96f9343079ee356e5024dba86d0b
Matheus Thurler <matheusthurlernf@gmail.com>
2025-03-18 17:34:21 -0300

    add page santander test and unit test

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -4,10 +4,8 @@ import Santander from "@/pages/santander/index.vue";
 
 
 describe('@/pages/santander/index.vue', () => {
-  it('renders the h1 with the correct text', () => {
+  it('renders the correct text', () => {
     const wrapper = mount(Santander);
-    const h1 = wrapper.find('h1');
-    expect(h1.exists()).toBe(true);
-    expect(h1.text()).toBe('Santander');
+    expect(wrapper.text()).toContain('Santander');
   });
 });
