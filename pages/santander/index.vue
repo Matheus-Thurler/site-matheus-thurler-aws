19502de4a644e8aa25398762e4000749c0dc6e61
Matheus Thurler <matheusthurlernf@gmail.com>
2025-03-18 20:11:50 -0300

    teste concluido

--- pages/santander/index.vue
+++ pages/santander/index.vue
@@ -2,7 +2,7 @@
   <div class="mt-20">
     <div class="flex flex-wrap justify-center p-2 pb-20">
       <h1>Santander</h1>
-      <button @click="handleClick">Click me</button>
+      <button @click="handleClick" type="button" aria-label="Clique para ação">Click me</button>
     </div>
   </div>
 </template>
