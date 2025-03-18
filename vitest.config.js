98dd084acf51c453cd28d150f378925509747c74
Matheus Thurler <44201662+Matheus-Thurler@users.noreply.github.com>
2025-03-18 18:12:21 -0300

    test

--- vitest.config.js
+++ vitest.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
+    provider: 'istanbul',
     include: ['tests/unit/**/*.spec.js'],
     coverage: {
       reporter: ['html', 'lcov', 'text-summary'],
